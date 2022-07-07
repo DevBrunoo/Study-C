c1763c52b1782c1702fac48de12466370f62be08
Bruno Oliveira <brunocosta1699320@gmail.com>
2022-07-07 19:16:47 -0300

    1.0

---
+++ data-types.c
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+nomespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 10;
+            decimal salario = 1234.56; 
+        }
+    }
+}
