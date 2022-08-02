b34e67fbaf14b6a95d4952525afae2567ce21ead
Bruno Oliveira <brunocosta1699320@gmail.com>
2022-08-01 23:55:48 -0300

    1.0

---
+++ boleaneas.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+  
+int main(void)
+{
+    string s = "Pocuntador 1 esta com 500 pontos!";
+    string t = "Pocuntador 2 esta com 300 pontos!";
+    string b = "Pocuntador 2 esta com 900 pontos!";
+  
+    printf("%s\n", s);
+    printf("%s\n", t);
+    printf("%s\n", b);
+}
