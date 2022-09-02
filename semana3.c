8ba9582e936ea246ba3d99d8f5d27ceaede74930
Bruno Oliveira <brunocosta1699320@gmail.com>
2022-09-01 22:58:47 -0300

    2

---
+++ semana3.c
@@ -0,0 +1,12 @@
+
+If no doors 
+ Return false
+If number behind middle door
+ Return true
+Else if number < middle door
+ Search left half
+
+ if __nonnull doors
+  return false;
+if number behind middle door
+ 
