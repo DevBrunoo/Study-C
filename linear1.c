d1a0ed4f90901bd5d09fca642cf9ea1805cfa985
Bruno Oliveira <brunocosta1699320@gmail.com>
2022-09-05 23:04:20 -0300

    2

---
+++ linear1.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+void main()
+{
+
+If no doors
+    Return false
+If number behind doors[middle]
+    Return true
+Else if number < doors[middle]
+    Search doors[0] through doors[middle - 1]
+Else if number > doors[middle]
+    Search doors [middle + 1] through doors[n - 1]
+
+
+}
+If no doors
+    Return false
+If number behind middle door
+    Return true
+Else if number < middle door
+    Search left half
+Else if number > middle door
+    Search right half
+
+
+    
